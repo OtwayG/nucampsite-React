1a5d947b27bb632f1aa755a979fbbcb4a102d302
George Otway <georgeotway@gmail.com>
2021-06-26 08:33:51 -0700

    week 3 daily work

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+//impote = as serviceWorker from './serviveWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import 'font-awesome/css/font-awesome.css';
+
 import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
+import { findRenderedComponentWithType } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
 ReactDOM.render(
   <React.StrictMode>
